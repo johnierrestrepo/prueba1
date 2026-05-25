4982e3bb712e736bc2f3366bfe92ae1f3cb7c3a5
johnierrestrepo <jhonieralex1998@gmail.com>
2026-05-25 07:29:35 -0500

    ingresar apellido

--- prueba.py
+++ prueba.py
@@ -5,3 +5,7 @@ print(f"Hola, {nombre}! Tienes {edad} años.")
 
 if int(edad) < 18:
     print("Eres menor de edad.")
+
+apellido = input("cual es tu apellido")
+if nombre == apellido:
+ print("nombre correcto")
